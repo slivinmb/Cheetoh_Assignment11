f6c3fe634d1d7e88c27dc959f59acd16c40e2d2f
Michael Slivinski <mbslivinski@gmail.com>
2025-04-15 13:31:23 -0400

    Added zipPackage for the zip code cleaning.

---
+++ Cheetoh_Assignment11/zipPackage/zipCode.py
@@ -0,0 +1,13 @@
+# File Name : zipCode.py
+# Student Name: Michael Slivinski and Will Claus
+# email: slivinmb@mail.uc.edu
+# Assignment Number: Assignment 11
+# Due Date:   4/17/2025
+# Course #/Section:   IS4010-001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  
+
+# Brief Description of what this module does: 
+# Citations: 
+
+# Anything else that's relevant:
